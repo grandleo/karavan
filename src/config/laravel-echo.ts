40869810ef8d108ccda8f092f5cc800263509b3b
Александр Черныш <c.alexcer93@yandex.ru>
2024-05-12 21:37:53 +0300

    Фикс laravel echo

--- src/config/laravel-echo.ts
+++ src/config/laravel-echo.ts
@@ -1,31 +1,88 @@
+// import Echo from "laravel-echo";
+// import Pusher from "pusher-js";
+//
+// declare global {
+//     interface Window {
+//         Pusher: typeof Pusher;
+//     }
+// }
+//
+// window.Pusher = require("pusher-js");
+//
+// const echoConfig = {
+//     broadcaster: 'pusher',
+//     key: process.env.NEXT_PUBLIC_WEBSOCKET_KEY,
+//     cluster: process.env.NEXT_PUBLIC_WEBSOCKET_CLUSTER,
+//     encrypted: false,
+//     forceTLS: false,
+//     wsPort: 6001,
+//     wsHost: process.env.NEXT_PUBLIC_WEBSOCKET_URL,
+//     // Дополнительные настройки, если нужно
+//     // authEndpoint: '/broadcasting/auth',
+//     // auth: {
+//     //   headers: {
+//     //     Authorization: `Bearer ${YOUR_AUTH_TOKEN}`,
+//     //   },
+//     // },
+// };
+//
+// const echo = new Echo(echoConfig);
+//
+// export default echo;
+
+
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 
-declare global {
-    interface Window {
-        Pusher: typeof Pusher;
-    }
-}
+// declare global {
+//     interface Window {
+//         Pusher: typeof Pusher;
+//     }
+// }
 
-window.Pusher = require("pusher-js");
+let echo;
 
-const echoConfig = {
+// if (typeof window !== 'undefined') {
+const pusher = new Pusher( '2311', {
+    cluster: 'eu'
+})
+
+echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.NEXT_PUBLIC_WEBSOCKET_KEY,
-    cluster: process.env.NEXT_PUBLIC_WEBSOCKET_CLUSTER,
+    cluster: 'eu',
     encrypted: false,
     forceTLS: false,
     wsPort: 6001,
-    wsHost: process.env.NEXT_PUBLIC_WEBSOCKET_URL,
-    // Дополнительные настройки, если нужно
-    // authEndpoint: '/broadcasting/auth',
+    // authEndpoint: 'http://localhost:8000/api/broadcasting/auth',
     // auth: {
-    //   headers: {
-    //     Authorization: `Bearer ${YOUR_AUTH_TOKEN}`,
-    //   },
+    //     headers: {
+    //         'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
+    //     }
     // },
-};
+    client: pusher
+})
+// }
 
-const echo = new Echo(echoConfig);
+// window.Pusher = require("pusher-js");
+
+// const echoConfig = {
+//     broadcaster: 'pusher',
+//     key: process.env.NEXT_PUBLIC_WEBSOCKET_KEY,
+//     cluster: process.env.NEXT_PUBLIC_WEBSOCKET_CLUSTER,
+//     encrypted: false,
+//     forceTLS: false,
+//     wsPort: 6001,
+//     wsHost: process.env.NEXT_PUBLIC_WEBSOCKET_URL,
+// Дополнительные настройки, если нужно
+// authEndpoint: '/broadcasting/auth',
+// auth: {
+//   headers: {
+//     Authorization: `Bearer ${YOUR_AUTH_TOKEN}`,
+//   },
+// },
+// };
+
+// const echo = new Echo(echoConfig);
 
 export default echo;
