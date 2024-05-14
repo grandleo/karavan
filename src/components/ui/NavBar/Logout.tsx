8e6edb2126217ec7dc7c603a7a0cd80ace235573
Александр Черныш <c.alexcer93@yandex.ru>
2024-05-14 18:56:22 +0300

    Фикс кнопки выхода

--- src/components/ui/NavBar/Logout.tsx
+++ src/components/ui/NavBar/Logout.tsx
@@ -1,11 +1,12 @@
 import {Button, Text} from "@mantine/core";
 import {IconLogout} from "@tabler/icons-react";
 import classes from "./navbar.module.css";
+import {logout} from "@/helpers/auth";
 
 const Logout = () => {
     return (
         <>
-            <Button variant="outline" fullWidth className={classes.logout} onClick={() => console.log('test')}>
+            <Button variant="outline" fullWidth className={classes.logout} onClick={() => logout(true)}>
                 <IconLogout /> <Text className={classes.logoutText}>Выйти</Text>
             </Button>
         </>
