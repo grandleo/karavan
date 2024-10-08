fa067c739ab467a8e0923e58ff1525daa68535dc
Александр Черныш <c.alexcer93@yandex.ru>
2024-10-09 01:01:21 +0300

    Удалил лишнею папку

--- src/components/pages/supplier/stock/index.tsx
+++ src/components/pages/supplier/stock/index.tsx
@@ -1,12 +1,10 @@
 'use client'
 
 import SimplePage from "@/components/simplePage";
-import {StockForm, SupplierStockList, Timer} from "@/components/stock";
+import {StockForm, SupplierStockList} from "@/components/stock";
 import {useFetchSupplierStockQuery} from "@/store/api/supplier/stockSupplier.api";
-import {useParams} from "next/navigation";
 import React, {useEffect, useState} from "react";
-import {Flex, Group, Select, SelectProps} from "@mantine/core";
-import {IconCheck} from "@tabler/icons-react";
+import {Flex} from "@mantine/core";
 import WarehouseSelector from "@/features/warehouses/components/WarehouseSelector";
 import {useWarehouse} from "@/features/warehouses/providers/WarehouseProvider";
 
