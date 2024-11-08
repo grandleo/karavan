5cb33fd4501e5293c111f9cc003b49d500d65b86
Александр Черныш <c.alexcer93@yandex.ru>
2024-11-09 00:11:11 +0300

    Фикс

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,7 +1,4 @@
 import {Metadata} from "next";
-import {HeaderHome} from "@/components/ui/header";
-import {Container} from "@mantine/core";
-import {HomeFooter} from "@/components/ui/Footer";
 import PrivacyPage from "@/screens/PrivacyPage";
 
 export const metadata: Metadata = {
@@ -11,13 +8,7 @@ export const metadata: Metadata = {
 export default function Page() {
     return (
         <>
-            <HeaderHome/>
-
-            <Container size="md" mt={120}>
-                <PrivacyPage/>
-            </Container>
-
-            <HomeFooter/>
+            <PrivacyPage/>
         </>
     )
 }
