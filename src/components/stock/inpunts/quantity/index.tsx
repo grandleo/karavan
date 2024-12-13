a0afd184707d6e4a44dc4dadef962f22a3df5f8a
Александр Черныш <c.alexcer93@yandex.ru>
2024-12-13 21:56:46 +0300

    Мелкие изменения

--- src/components/stock/inpunts/quantity/index.tsx
+++ src/components/stock/inpunts/quantity/index.tsx
@@ -1,5 +1,5 @@
 import {KeyboardEvent, useState, FocusEvent} from 'react';
-import {NumberInput} from "@mantine/core";
+import {Image, NumberInput} from "@mantine/core";
 import {Controller, useFormContext} from "react-hook-form";
 import classes from "@/components/stock/styles.module.css";
 
@@ -8,18 +8,7 @@ const QuantityInput = ({width, handleUpdate}: QuantityInputTypes) => {
     const [isFocused, setIsFocused] = useState(false);
 
     const icon = (
-        <svg
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                d="M14.3999 2.40002H8.3999C7.7369 2.40002 7.1999 2.93702 7.1999 3.60002V6.00002C7.1999 6.66302 6.6629 7.20002 5.9999 7.20002H3.5999C2.9369 7.20002 2.3999 7.73702 2.3999 8.40002V14.4C2.3999 15.063 2.9369 15.6 3.5999 15.6H14.3999C15.0629 15.6 15.5999 15.063 15.5999 14.4V3.60002C15.5999 2.93702 15.0629 2.40002 14.3999 2.40002ZM11.9999 10.2C11.9999 11.1924 11.1923 12 10.1999 12H7.1999V13.8L4.7999 11.4L7.1999 9.00002V10.8H10.1999C10.5305 10.8 10.7999 10.5312 10.7999 10.2V6.60002C10.7999 6.26822 11.0687 6.00002 11.3999 6.00002C11.7311 6.00002 11.9999 6.26822 11.9999 6.60002V10.2Z"
-                fill="#C1C6D0"
-            />
-        </svg>
+        <Image src="/icons/enter.svg" width={18} height={18} />
     );
 
     const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
