b44c61ea28423b4d6567ef14eb2c4545f9a2d75f
Александр Черныш <c.alexcer93@yandex.ru>
2024-12-18 00:38:54 +0300

    Тест веб версии

--- src/app/telegram/page.tsx
+++ src/app/telegram/page.tsx
@@ -1,30 +1,29 @@
 "use client";
 import { useEffect, useState } from "react";
-// import { useSearchParams } from "next/navigation";
 
 export default function TelegramWebApp() {
-    // const searchParams = useSearchParams();
     const [chatId, setChatId] = useState<string | null>(null);
     const [token, setToken] = useState<string | null>(null);
 
-    // useEffect(() => {
-        // Получаем chat_id и token из URL параметров
-        // const chat_id = searchParams.get("chat_id");
-        // const token = searchParams.get("token");
+    useEffect(() => {
+        // Получаем параметры из URL с использованием window.location
+        const searchParams = new URLSearchParams(window.location.search);
+        const chat_id = searchParams.get("chat_id");
+        const bot_token = searchParams.get("token");
 
-        // if (chat_id && token) {
-        //     setChatId(chat_id);
-        //     setToken(token);
-        // }
-    // }, [searchParams]);
+        if (chat_id && bot_token) {
+            setChatId(chat_id);
+            setToken(bot_token);
+        }
+    }, []);
 
     return (
         <div style={{ textAlign: "center", padding: "20px" }}>
             <h1>Telegram WebApp</h1>
             {chatId && token ? (
                 <>
-                    <p>Ваш chat_id</p>
-                    <p>Ваш токен</p>
+                    <p>Ваш chat_id: {chatId}</p>
+                    <p>Ваш токен: {token}</p>
                 </>
             ) : (
                 <p>Загрузка данных...</p>
